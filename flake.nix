8512b59021e1820ba50469fafb7cc2bf5edf81d2
Marco Moschettini <m.marmos@gmail.com>
2026-01-16 15:05:24 +0100

    chore: bump version to v0.3.1

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
     }:
     let
       # Version configuration - update this for releases
-      version = "0.3.0";
+      version = "0.3.1";
     in
     flake-utils.lib.eachDefaultSystem (
       system:
