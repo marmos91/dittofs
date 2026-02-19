c451b4c24b8d14559d7f592ba725401aff42ec4f
Marco Moschettini <m.marmos@gmail.com>
2026-02-19 14:43:59 +0100

    fix: fix homebrew release

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
     }:
     let
       # Version configuration - update this for releases
-      version = "0.6.1";
+      version = "0.6.2";
     in
     flake-utils.lib.eachDefaultSystem (
       system:
