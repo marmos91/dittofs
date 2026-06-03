2beda0f2626c80edd6e286635a1242009ae699dc
Marco Moschettini <marco.moschettini@cubbit.io>
2026-06-03 13:55:29 +0200

    chore(release): v0.17.0

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
     }:
     let
       # Version configuration - update this for releases
-      version = "0.16.0";
+      version = "0.17.0";
     in
     flake-utils.lib.eachDefaultSystem (
       system:
